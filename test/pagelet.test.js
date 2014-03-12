435c9a9337554f3db285d63fb00d7e50f8fe251c
Swaagie <info@martijnswaagman.nl>
2014-03-13 00:25:02 +0100

    [fix] test

--- test/pagelet.test.js
+++ test/pagelet.test.js
@@ -15,7 +15,7 @@ describe('Pagelet', function () {
   });
 
   it('rendering is asynchronously', function (done) {
-    pagelet.render(pagelet.emits('called'));
+    pagelet.get(pagelet.emits('called'));
     // Listening only till after the event is potentially emitted, will ensure
     // callbacks are called asynchronously by pagelet#render.
     pagelet.on('called', done);
