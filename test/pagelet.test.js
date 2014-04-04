f6c9012cb9589b37654e6814d353399109b6abe7
3rd-Eden <info@3rd-Eden.com>
2014-04-04 18:43:44 +0200

    [test] Added test for Pagelet.on()

--- test/pagelet.test.js
+++ test/pagelet.test.js
@@ -14,6 +14,21 @@ describe('Pagelet', function () {
     pagelet = null;
   });
 
+  describe('.on', function () {
+    it('sets the pathname', function () {
+      var pagelet = Pagelet.extend({});
+      expect(pagelet.prototype.directory).to.equal('');
+
+      pagelet.prototype.directory = 'foo';
+      expect(pagelet.prototype.directory).to.equal('foo');
+
+      pagelet.on(module);
+
+      expect(pagelet.prototype.directory).to.be.a('string');
+      expect(pagelet.prototype.directory).to.equal(__dirname);
+    });
+  });
+
   it('rendering is asynchronously', function (done) {
     pagelet.get(pagelet.emits('called'));
     // Listening only till after the event is potentially emitted, will ensure
