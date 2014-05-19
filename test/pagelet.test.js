98237142fb83c9c9acb2702a352084d4bf051ca9
3rd-Eden <info@3rd-Eden.com>
2014-05-19 17:54:54 +0200

    [test] Adding tests for the optimizer

--- test/pagelet.test.js
+++ test/pagelet.test.js
@@ -2,37 +2,70 @@ describe('Pagelet', function () {
   'use strict';
 
   var Pagelet = require('../').extend({ name: 'test' })
-    , chai = require('chai')
-    , expect = chai.expect
-    , pagelet;
+    , assume = require('assume')
+    , pagelet
+    , P;
 
   beforeEach(function () {
-    pagelet = new Pagelet;
+    P = Pagelet.extend({
+      directory: __dirname,
+      view: 'fixtures/view.html'
+    });
+
+    pagelet = new P();
   });
 
   afterEach(function () {
     pagelet = null;
   });
 
-  describe('.on', function () {
-    it('sets the pathname', function () {
-      var pagelet = Pagelet.extend({});
-      expect(pagelet.prototype.directory).to.equal('');
-
-      pagelet.prototype.directory = 'foo';
-      expect(pagelet.prototype.directory).to.equal('foo');
-
-      pagelet.on(module);
-
-      expect(pagelet.prototype.directory).to.be.a('string');
-      expect(pagelet.prototype.directory).to.equal(__dirname);
-    });
-  });
-
   it('rendering is asynchronously', function (done) {
     pagelet.get(pagelet.emits('called'));
+
     // Listening only till after the event is potentially emitted, will ensure
     // callbacks are called asynchronously by pagelet#render.
     pagelet.on('called', done);
+  });
+
+  describe('.on', function () {
+    it('sets the pathname', function () {
+      var pagelet = Pagelet.extend({});
+      assume(pagelet.prototype.directory).to.equal('');
+
+      pagelet.prototype.directory = 'foo';
+      assume(pagelet.prototype.directory).to.equal('foo');
+
+      pagelet.on(module);
+
+      assume(pagelet.prototype.directory).to.be.a('string');
+      assume(pagelet.prototype.directory).to.equal(__dirname);
+    });
+  });
+
+  describe('.optimize', function () {
+    it('is a function', function () {
+      assume(Pagelet.optimize).to.be.a('function');
+      assume(P.optimize).to.be.a('function');
+      assume(Pagelet.optimize).to.equal(P.optimize);
+    });
+
+    it('resolves the view', function () {
+      assume(P.prototype.view).to.equal('fixtures/view.html');
+      P.optimize();
+
+      assume(P.prototype.view).to.equal(__dirname +'/fixtures/view.html');
+    });
+
+    it('prefetches the `view`');
+    it('resolves the `error` view');
+    it('prefetches the `error` view');
+    it('transforms `css` in to an array');
+    it('resolves the `css` files in to an array');
+    it('resolves the `js` files in to an array');
+    it('only resolves non http/https dependencies');
+    it('allows rpc as a string');
+    it('allows lowercase rpc');
+    it('stores all introduced properties as array');
+    it('adds a freelist factory');
   });
 });
